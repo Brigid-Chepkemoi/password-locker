08245e90fc4149ba0fe363e767e8cd678c79fdca
brie <briememo95@gmail.com>
2022-04-23 11:43:56 +0300

    add first commit

---
+++ credentials.py
@@ -0,0 +1,5 @@
+class Credentials:
+
+    def _init_(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+
+     user_List=[]
+
+    def _init_(self,username,email):
+        self.username=username
+        self.email=email
+
+if User.username == username
