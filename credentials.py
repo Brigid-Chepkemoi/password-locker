40340ea7a35b0862dfbbdd054adccdd118c03b88
brie <briememo95@gmail.com>
2022-04-24 22:23:44 +0300

    add delete credentials

--- credentials.py
+++ credentials.py
@@ -1,18 +1,29 @@
+import os
+from time import sleep
+
+
 class Credentials(object):
     system = ''
     username = ""
     password = ""
+    created_by = ""
+    platform = ""
 
-    def __init__(self, username, password, system):
+    def __init__(self, created_by, username, password, platform):
+        self.created_by = created_by
         self.username = username
         self.password = password
-        self.system = system
+        self.platform = platform
 
     def create_credentials(self, credentials_list):
-        if credentials_list.__contains__(self.username):
+        if credentials_list.__contains__(self.created_by+'\t \t'+self.username+'\t \t'+self.password+'\t \t'+self.platform):
             print("Account already exists")
         else:
-            credentials_list.append(self)
+            credentials_list.append(self.created_by+' '+self.username+' '+self.password+' '+self.platform)
+            handle = open("/home/brie/dat2.txt", "a")
+            handle.write(self.created_by+'\t \t'+self.username+'\t \t'+self.password+'\t \t'+self.platform + '\n')
+            handle.close()
+            print("Credentials added successfully")
 
     def modify_user(self, username, email):
         for user in self.user_list:
@@ -28,3 +39,28 @@ class Credentials(object):
                 self.user_list.remove(user)
             else:
                 print("User doesn't exist")
+
+    def show_credentials(self, credentials_list, authenticated_user):
+        """
+        method to show credentials for logged-in user
+        """
+        print('you'+'\t \t'+'account'+'\t'+"\tpassword"+'\t'+"platform")
+        for user in credentials_list:
+            if user.startswith(authenticated_user):
+                print(user)
+
+    def delete_credentials(self, authenticated_user, account, platform):
+        """
+        method to delete credentials
+        """
+        input_file = open('/home/brie/dat2.txt', "r")
+        output_file = open('/home/brie/dat3.txt', "w")
+        output_file.write(input_file.readline())
+        for line in input_file:
+            if not line.lstrip().startswith(authenticated_user+"\t \t"+account) and not line.lstrip().endswith(platform):
+                output_file.write(line)
+        input_file.close()
+        output_file.close()
+        os.rename('/home/brie/dat3.txt','/home/brie/dat2.txt')
+        print('Account deleted')
+
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import os
+from time import sleep
+
 from credentials import Credentials
 from user import User
 
@@ -5,24 +8,62 @@ from user import User
 class Main:
     credentials_list = []
     user_list = []
-    # def authenticate(self):
+    authenticated_user = ""
+    authenticated_password = ""
+    cred_name = ""
+    cred_password = ""
+    cred_platform = ""
+
+    def authenticate(self):
+        self.screen_clear()
+        login = input("Enter username: ")
+        password = input("Enter password: ")
+        user = login + ' ' + password
+        if user in self.user_list:
+            self.authenticated_user = login
+            self.authenticated_password = password
+            self.credentials_menu()
+        else:
+            print("Wrong username and password combination ")
+            sleep(5)
+            self.authenticate()
 
     def main_menu(self):
-
+        self.screen_clear()
+        self.reload_data()
         print("Press 1 to create a user")
         print("Press 2 to login to a  user account")
         next_action = input("select next action: ")
         if next_action == '1':
             self.create_user()
-        # elif next_action == '2':
-            #self.authenticate()
+        elif next_action == '2':
+            self.authenticate()
         else:
             exit()
+
+    def credentials_menu(self):
+        self.screen_clear()
+        print("Press 1 to show your credentials")
+        print("Press 2 to add a new credentials to your list")
+        print("Press 3 to delete credentials")
+        action = input("select next action: ")
+        credentials = Credentials(self.authenticated_user, self.cred_name, self.cred_password, self.cred_platform)
+        if action == "1":
+            credentials.show_credentials(self.credentials_list, self.authenticated_user)
+        elif action == "2":
+            self.create_credentials()
+        elif action == "3":
+            accnt_to_del = input("Enter the account name to delete")
+            pltfrm_to_del = input("Enter the platform for the account to delete")
+            credentials.delete_credentials(self.authenticated_user, accnt_to_del, pltfrm_to_del)
+        else:
+            self.credentials_menu()
 
     def create_user(self):
         """
         this method creates users for our application
           """
+        self.screen_clear()
         login = input("Enter username: ")
         auth = input("Enter Password: ")
         auth2 = input("Confirm Password: ")
@@ -32,11 +73,37 @@ class Main:
         # self.main_menu()
 
     def create_credentials(self):
+        """
+        Method to create new credentials
+        """
+        self.screen_clear()
         username = input("Enter username: ")
         password = input("Enter Password: ")
-        credentials = Credentials(username, password)
+        platform = input("Enter the platform to which these credentials belong: ")
+        credentials = Credentials(self.authenticated_user, username, password, platform)
         credentials.create_credentials(self.credentials_list)
         self.main_menu()
+
+    def screen_clear(self):
+        if os.name == 'posix':
+            _ = os.system('clear')
+        else:
+            _ = os.system('cls')
+
+    def reload_data(self):
+        self.user_list=[]
+        self.credentials_list=[]
+        with open("/home/brie/dat1.txt") as file:
+            for line in file:
+                line = line.strip()
+                self.user_list.append(line)
+        file.close()
+
+        with open("/home/brie/dat2.txt") as file:
+            for line in file:
+                line = line.strip()
+                self.credentials_list.append(line)
+        file.close()
 
 
 if __name__ == '__main__':
