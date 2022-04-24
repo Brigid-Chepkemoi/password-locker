27bd516ff36d4e12055ba75f34ab6eff3a78b6f4
brie <briememo95@gmail.com>
2022-04-25 00:12:43 +0300

    validate tests

--- user_test.py
+++ user_test.py
@@ -1,10 +1,28 @@
 import unittest
+
+
+
 from user import User
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.user_list = self.user_list("James", "p1", "p1")
+
+    def test_init(self):
+
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.user_list.username, "James")
+        self.assertEqual(self.user_list.password, "p1")
+        self.assertEqual(self.user_list.password2, "p1")
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+        unittest.main()
