88b679b0fadd0e2d054ab866953f8952deaf1936
brie <briememo95@gmail.com>
2022-04-25 01:03:26 +0300

    add show credentials functionality

--- user_test.py
+++ user_test.py
@@ -22,14 +22,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user_list.password, "p1")
         self.assertEqual(self.user_list.password2, "p1")
 
-   def show_credentials(self, credentials_list, authenticated_user):
-        """
-        method to show credentials for logged-in user
-        """
-        print('you' + '\t \t' + 'account' + '\t' + "\tpassword" + '\t' + "platform")
-        for user in credentials_list:
-            if user.startswith(authenticated_user):
-                print(user)
+
 
 if __name__ == '__main__':
         unittest.main()
